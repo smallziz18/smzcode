41851b5b0527388212f9a08dc7d44ee29adf1ad4
smallziz <abdaziz.diouf@astarex-ps.com>
2026-05-26 10:13:54 +0000

    add theme management and dialog components with command actions

--- packages/cli/src/components/status-bar.tsx
+++ packages/cli/src/components/status-bar.tsx
@@ -1,14 +1,16 @@
-import {TextAttributes} from "@opentui/core";
+import { TextAttributes } from "@opentui/core";
+import { useTheme } from "../providers/theme";
 
-export function StatusBar(){
+export function StatusBar() {
+    const { colors } = useTheme();
+
     return (
         <box flexDirection="row" gap={1}>
-            <text fg="cyan"> Build</text>
-                <text attributes={TextAttributes.DIM} fg="gray">
-                    &gt;
-                </text>
-                <text>opus-4-6</text>
-
+            <text fg={colors.primary}>Build</text>
+            <text attributes={TextAttributes.DIM} fg={colors.dimSeparator}>
+                ›
+            </text>
+            <text>opus-4-6</text>
         </box>
-    )
+    );
 }
